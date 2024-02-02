659e9f88deb0fccab93316fa3214ddcf72f9aff3
bruno-connectabil <pamoroso.bruno@gmail.com>
2024-02-02 15:49:16 -0300

    Flash message working on register user

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -3,22 +3,33 @@ import api from "../utils/api";
 
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import useFlashMessage from "./useFlashMessage";
 
 interface IAuth{
   register: (user: Object) => Promise<void>;
 }
 
 export default function useAuth(): IAuth {
+
+  const {setFlashMessage} = useFlashMessage();
+
   async function register(user: Object) {
+
+    let msgText = 'Cadastro realizado com sucesso!';
+    let msgType = 'success';
+
     try {
       const data = await api.post("/users/register", user).then((response) => {
         return response.data;
       });
 
       console.log(data);
-    } catch (err) {
-      console.log(err);
+    } catch (err: any) {
+      msgText = err.response.data.message;
+      msgType = 'error'
     }
+
+    setFlashMessage(msgText, msgType);
   }
 
   return { register };
