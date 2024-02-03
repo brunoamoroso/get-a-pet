6fb919365eeb4c4be38c2c9db9ef472d1666a5aa
bruno-connectabil <pamoroso.bruno@gmail.com>
2024-02-03 10:34:15 -0300

    Login form layout

--- frontend/src/components/pages/auth/Login.tsx
+++ frontend/src/components/pages/auth/Login.tsx
@@ -1,14 +1,28 @@
-import * as React from 'react';
+import { useState, useContext } from "react";
+import Input from "../../form/Input";
+import styles from '../../form/Form.module.css';
 
-interface ILoginProps {
-}
+// Context
+import { Context } from "../../../context/UserContext";
+import { Link } from "react-router-dom";
 
-const Login: React.FunctionComponent<ILoginProps> = (props) => {
+export interface ILoginProps {}
+
+export default function Login(props: ILoginProps) {
+  
+  function handleChange(){
+
+  }
+
   return (
-    <section>
-        <h1>Login</h1>
+    <section className={styles.form_container}>
+      <h1>Login</h1>
+      <form>
+        <Input text="E-mail" type="email" name="email" placeholder="Digite o seu e-mail" handleOnChange={handleChange}/>
+        <Input text="Senha"  type="password" name="password" placeholder="Digite a sua senha" handleOnChange={handleChange}/>
+        <input type="submit" value="Entrar" />
+      </form>
+      <p>Não tem conta? <Link to="/register">Clique aqui.</Link></p>
     </section>
   );
-};
-
-export default Login;
+}
