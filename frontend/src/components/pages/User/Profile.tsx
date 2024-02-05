a44497344aa8f7b5465dfdf3e90a6ef8b75e2a58
bruno-connectabil <pamoroso.bruno@gmail.com>
2024-02-05 16:33:12 -0300

    Profile page layout done

--- frontend/src/components/pages/User/Profile.tsx
+++ frontend/src/components/pages/User/Profile.tsx
@@ -1,10 +1,69 @@
-export interface IProfileProps {
-}
+import { useState, useEffect } from "react";
+import formStyles from "../../form/Form.module.css";
+import Input from "../../form/Input";
+import styles from "./Profile.module.css";
 
-export default function Profile (props: IProfileProps) {
+export default function Profile() {
+  const [user, setUser] = useState({});
+
+  function onFileChange() {}
+
+  function handleChange() {}
+
+  function handleSubmit() {}
   return (
     <section>
-        <h1>Profile</h1>
+      <div className={styles.profile_header}>
+        <h1>Perfil</h1>
+        <p>Imagem</p>
+      </div>
+      <form onSubmit={handleSubmit} className={formStyles.form_container}>
+        <Input
+          text="Imagem"
+          type="file"
+          name="image"
+          handleOnChange={onFileChange}
+        />
+        <Input
+          text="Email"
+          type="email"
+          name="email"
+          placeholder="Digite o seu email"
+          handleOnChange={handleChange}
+          value={user.email || ""}
+        />
+        <Input
+          text="Nome"
+          type="text"
+          name="name"
+          placeholder="Digite o seu nome"
+          handleOnChange={handleChange}
+          value={user.name || ""}
+        />
+        <Input
+          text="Telefone"
+          type="text"
+          name="phone"
+          placeholder="Digite o seu telefone"
+          handleOnChange={handleChange}
+          value={user.phone || ""}
+        />
+        <Input
+          text="Senha"
+          type="password"
+          name="password"
+          placeholder="Digite sua senha"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="Confirmação de Senha"
+          type="password"
+          name="confirmpassword"
+          placeholder="Confirme a sua senha"
+          handleOnChange={handleChange}
+        />
+        <input type="submit" value="Editar" />
+      </form>
     </section>
   );
 }
