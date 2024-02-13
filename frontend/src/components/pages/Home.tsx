8de5132cb088d5bed5457c0eaa55c3d0b4bbef61
bruno-connectabil <pamoroso.bruno@gmail.com>
2024-02-13 15:36:40 -0300

    Some modifications to home and edit pets

--- frontend/src/components/pages/Home.tsx
+++ frontend/src/components/pages/Home.tsx
@@ -1,7 +1,20 @@
+import api from "../../utils/api";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import styles from './Home.module.css';
+
 export interface IHomeProps {
 }
 
 export default function Home (props: IHomeProps) {
+  const [pets, setPets] = useState([]);
+
+  useEffect(() => {
+    api.get('/pets').then(response => {
+      setPets(response.data.pets);
+    })
+  }, []);
+
   return (
     <section>
         <h1>Home</h1>
